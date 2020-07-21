aedc7acffc6205094ad37a2e24198ab0b1c53b28
Jan Haffner <info@jan-haffner.de>
2020-07-21 19:35:32 +0200

    [BUGFIX] Fix HTML header

--- views/header.php
+++ views/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html>
+<html lang="de">
 <head>
-    <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=0.95">
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="<?php echo CSS; ?>" media="screen" />
