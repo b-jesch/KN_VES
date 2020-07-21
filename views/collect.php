810c25ae98c88b87a32ba6f2d8a791f8cb9c7b73
Jan Haffner <info@jan-haffner.de>
2020-07-21 19:32:17 +0200

    [BUGFIX] Fix HTML view forms

--- views/collect.php
+++ views/collect.php
@@ -20,52 +20,113 @@ include HEADER;
                 ändern. Zur Identifikation wird Deine User-ID herangezogen.
             </p>
             <hr>
-            <table><tr><td class="desc_form"><label for="id">Kodinerds User ID: </td>
+            <table>
+                <tr>
+                    <td class="desc_form"><label for="id">Kodinerds User ID:</label></td>
                     <td>
                         <input type="text" class="short" name="id" id="id" form="n"
-                               value="<?php echo $_SESSION['id'];  ?>"readonly></label>
-                    </td></tr>
+                               value="<?php echo $_SESSION['id'];  ?>" readonly>
+                    </td>
+                </tr>
                 <tr><td colspan="2"><hr></td></tr>
-                <tr><td class="desc_form"><label for="event_date">Startdatum:</td>
-                    <td><input type="date" class="short" name="event_date" id="event_date" form="n" required></label>
-                    </td></tr>
-                <tr><td class="desc_form"><label for="event">Event:</td>
-                    <td><input type="text" class="short" name="event" id="event" form="n" required></label>
-                    </td></tr>
-                <tr><td class="desc_form"><label for="genre">Genre:</td>
-                    <td><input type="text" class="short" name="genre" id="genre" form="n" required></label>
-                    </td></tr>
-                <tr><td colspan="2"><hr></td></tr>
-                <tr><td class="desc_form"><label for="stream">Stream URL:</td>
-                    <td><input type="text" class="long" name="stream" id="stream" form="n"></label>
-                    </td></tr>
-                <tr><td class="desc_form"><label for="isyoutube">Youtube Link:</td>
-                    <td><input type="checkbox" name="isyoutube" id="isyoutube" form="n"></label>
-                    </td></tr>
-                <tr><td class="desc_form"><label for="from">gültig ab:</td>
-                    <td><input type="datetime-local" class="short" name="from" id="from" form="n"></label>
-                    </td></tr>
-                <tr><td class="desc_form"><label for="to">gültig bis:</td>
-                    <td><input type="datetime-local" class="short" name="to" id="to" form="n"></label>
-                    </td></tr>
-                <tr><td class="desc_form"><label for="to">dauerhaft gültig (Permastream):</td>
-                    <td><input type="checkbox" name="permalink" id="permalink" form="n"></label>
-                    </td></tr>
-                <tr><td class="desc_form"><label for="icon">Icon URL:</td>
-                    <td><input type="text" class="long" name="icon" id="icon" form="n"></label>
-                    </td></tr>
-                <tr><td class="desc_form"><label for="fanart">Poster URL:</td>
-                    <td><input type="text" class="long" name="fanart" id="fanart" form="n"></label>
-                    </td></tr>
-                <tr><td class="desc_form"><label for="plot">Beschreibung:</td>
-                    <td><textarea name="plot" id="plot" form="n" placeholder="Plot..."></textarea></label>
-                    </td></tr>
-
-                <tr><td colspan="2" class="center">
+                <tr>
+                    <td class="desc_form">
+                        <label for="event_date">Startdatum:</label>
+                    </td>
+                    <td>
+                        <input type="date" class="short" name="event_date" id="event_date" form="n" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="desc_form">
+                        <label for="event">Event:</label>
+                    </td>
+                    <td>
+                        <input type="text" class="short" name="event" id="event" form="n" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="desc_form">
+                        <label for="genre">Genre:</label>
+                    </td>
+                    <td>
+                        <input type="text" class="short" name="genre" id="genre" form="n" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2"><hr></td>
+                </tr>
+                <tr>
+                    <td class="desc_form">
+                        <label for="stream">Stream URL:</label>
+                    </td>
+                    <td>
+                        <input type="text" class="long" name="stream" id="stream" form="n">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="desc_form">
+                        <label for="isyoutube">Youtube Link:</label>
+                    </td>
+                    <td>
+                        <input type="checkbox" name="isyoutube" id="isyoutube" form="n">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="desc_form">
+                        <label for="from">gültig ab:</label>
+                    </td>
+                    <td>
+                        <input type="datetime-local" class="short" name="from" id="from" form="n">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="desc_form">
+                        <label for="to">gültig bis:</label>
+                    </td>
+                    <td>
+                        <input type="datetime-local" class="short" name="to" id="to" form="n">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="desc_form">
+                        <label for="permalink">dauerhaft gültig (Permastream):</label>
+                    </td>
+                    <td>
+                        <input type="checkbox" name="permalink" id="permalink" form="n">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="desc_form">
+                        <label for="icon">Icon URL:</label>
+                    </td>
+                    <td>
+                        <input type="text" class="long" name="icon" id="icon" form="n">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="desc_form">
+                        <label for="fanart">Poster URL:</label>
+                    </td>
+                    <td>
+                        <input type="text" class="long" name="fanart" id="fanart" form="n">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="desc_form">
+                        <label for="plot">Beschreibung:</label>
+                    </td>
+                    <td>
+                        <textarea name="plot" id="plot" form="n" placeholder="Plot..."></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" class="center">
                         <input class="button" type="submit" name='add' value="Erstellen">
                         <input class="button" type="button" name='abort' value="Abbrechen"
                                onclick="document.location.href='<?php echo ROOT.'?abort'; ?>';">
-                    </td></tr>
+                    </td>
+                </tr>
             </table>
         </div>
         <input type="hidden" name="site" value="collect_2">
