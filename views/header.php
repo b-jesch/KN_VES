a86c0bd7933c402d921c0ad334cf28b630c2dd40
Birger Jesch <birger.jesch@gmail.com>
2020-07-27 20:12:37 +0200

    favicon added

--- views/header.php
+++ views/header.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="<?php echo CSS; ?>" media="screen" />
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
     <meta name="format-detection" content="telephone=yes">
     <meta name="description" content="Eventserver der Kodinerds Community"/>
     <meta name="keywords" content="Events, Livestream, Kodinerds" />
